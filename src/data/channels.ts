952fed7fd16b89b2d96d8481ca69e8d8d7b34462
Psycheeeu <csx067@gmail.com>
2026-05-31 22:04:59 +0800

    Add files via upload

--- src/data/channels.ts
+++ src/data/channels.ts
@@ -1131,7 +1131,6 @@ export const channels: Channel[] = [
     stream: {
       url: 'https://converse.nathcreqtives.com/channels/tap-action-flix/manifest.mpd?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiJwc3ljaGVlZSIsImlhdCI6MTc3OTU0MDYwMSwiZXhwIjoxNzg3MzE2NjAxLCJhY2NvdW50RXhwaXJlZCI6ZmFsc2UsImFjY291bnRFeHBpcmVzQXQiOjE3ODczMTY2MDEsImFsbG93ZWRPcmlnaW5zIjpbImh0dHBzOi8vcHN5Y2hlZWVmbGl4dXV1Lm5ldGxpZnkuYXBwIiwiaHR0cHM6Ly9wc3ljaGVlZWZsaXh1dXUudmVyY2VsLmFwcCIsImh0dHBzOi8vcHN5Y2hlZmxpeC5wYWdlcy5kZXYiXX0.G9RVqvRccbU9QyWSE8yu67IR2-2-yotmB1AyIa-Cheo',
       type: 'dash',
-      drm: { scheme: '', clearkeys: { '' : '' }, },
     },
     programs: noScheduleProgram('Tap ActionFlix', 'Entertainment'),
   },
