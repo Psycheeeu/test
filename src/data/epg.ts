08666fae252f1b05abb8170a45b699d987b86cfd
Psycheeeu <csx067@gmail.com>
2026-06-06 07:21:24 +0800

    Update epg.ts

--- src/data/epg.ts
+++ src/data/epg.ts
@@ -6,7 +6,6 @@ export const EPG_SOURCE_URLS = [
   'https://epgshare01.online/epgshare01/epg_ripper_PH1.xml.gz',
   'https://epgshare01.online/epgshare01/epg_ripper_PH2.xml.gz',
   'https://raw.githubusercontent.com/atone77721/CIGNAL_EPG/refs/heads/main/clickthecity_epg.xml',
-  'https://raw.githubusercontent.com/atone77721/AMZN_EPG/refs/heads/main/nowtv.xml',
   'https://raw.githubusercontent.com/atone77721/CIGNAL_EPG/refs/heads/main/cignal_epg.xml',
   'https://raw.githubusercontent.com/AqFad2811/epg/refs/heads/main/astro.xml',
   'https://raw.githubusercontent.com/AqFad2811/epg/refs/heads/main/unifitv.xml',
