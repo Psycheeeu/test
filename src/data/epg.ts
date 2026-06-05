3a6093577961e923580f0d141dd853a1e4ac6f8e
Psycheeeu <csx067@gmail.com>
2026-06-06 07:27:13 +0800

    Update epg.ts

--- src/data/epg.ts
+++ src/data/epg.ts
@@ -10,7 +10,7 @@ export const EPG_SOURCE_URLS = [
   'https://raw.githubusercontent.com/AqFad2811/epg/refs/heads/main/astro.xml',
   'https://raw.githubusercontent.com/AqFad2811/epg/refs/heads/main/unifitv.xml',
   'https://raw.githubusercontent.com/AqFad2811/epg/refs/heads/main/indonesia.xml',
-  'https://www.open-epg.com/files/singapore1.xml.gz',
+  'https://raw.githubusercontent.com/laleeroy/epg/c17822d42b8f995e0f9f802a03b4a290d1dd37a0/guide.xml',
   'https://gsat.atone77721.workers.dev/gsat.xml',
   'https://epgshare01.online/epgshare01/epg_ripper_SG1.xml.gz',
   'https://epgshare01.online/epgshare01/epg_ripper_DUMMY_CHANNELS.xml.gz',
