9a5f1b422392f098d47bae6ee5e2c02045603d42
Psycheeeu <csx067@gmail.com>
2026-06-06 06:05:55 +0800

    Update channels.ts

--- src/data/channels.ts
+++ src/data/channels.ts
@@ -871,7 +871,7 @@ export const channels: Channel[] = [
     name: 'tvN Movies Pinoy',
     category: 'Local Entertainment',
     logo: 'https://i.imgur.com/e9vo9Z8.png',
-    epgId: 'cg_tvnpre',
+    epgId: 'cg_tvnmovie',
     epgSource: 'none',
     stream: {
       url: 'https://converse.nathcreqtives.com/channels/tvn-movies-pinoy/manifest.mpd?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiJwc3ljaGVlZSIsImlhdCI6MTc4MDU3NTA1MywiZXhwIjoxNzg4MzUxMDUzLCJhY2NvdW50RXhwaXJlZCI6ZmFsc2UsImFjY291bnRFeHBpcmVzQXQiOjE3ODgzNTEwNTMsImFsbG93ZWRPcmlnaW5zIjpbImh0dHBzOi8vcHN5Y2hlZWVmbGl4dXV1Lm5ldGxpZnkuYXBwIiwiaHR0cHM6Ly9wc3ljaGVlZWZsaXh1dXUudmVyY2VsLmFwcCIsImh0dHBzOi8vcHN5Y2hlZmxpeC5wYWdlcy5kZXYiXX0.ngv2-3w4tcSIZR4J72tEsTZGQYt4iWRdVfFo3sQZa84',
