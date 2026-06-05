6055ac0aa0a6957adafa8cd65a47ff4098640099
Psycheeeu <csx067@gmail.com>
2026-06-06 05:32:23 +0800

    Update epg.ts

--- src/data/epg.ts
+++ src/data/epg.ts
@@ -6,9 +6,9 @@ export const EPG_SOURCE_URLS = [
   'https://epgshare01.online/epgshare01/epg_ripper_PH1.xml.gz',
   'https://epgshare01.online/epgshare01/epg_ripper_PH2.xml.gz',
   'https://raw.githubusercontent.com/atone77721/CIGNAL_EPG/refs/heads/main/clickthecity_epg.xml',
+  'https://www.open-epg.com/files/malaysia3.xml',
   'https://gsat.atone77721.workers.dev/gsat.xml',
   'https://epgshare01.online/epgshare01/epg_ripper_SG1.xml.gz',
-  'https://epgshare01.online/epgshare01/epg_ripper_MY1.xml.gz',
 ];
 
 // --- UTILS ---
