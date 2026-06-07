8e8da48d7bf93955ebb0a53a968eb441348214cf
Psycheeeu <csx067@gmail.com>
2026-06-07 13:56:27 +0800

    Update channels.ts

--- src/data/channels.ts
+++ src/data/channels.ts
@@ -183,10 +183,10 @@ export const channels: Channel[] = [
     name: 'A2Z',
     category: 'Local',
     logo: 'https://i.imgur.com/DVSTY3w.png',
-    epgId: 'A2Z.ph',
+    epgId: 'a2z.Channel.11.ph',
     epgSource: 'none',
     stream: {
-      url: 'https://converse.nathcreqtives.com/channels/a2z/manifest.mpd?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiJwc3ljaGVlZSIsImlhdCI6MTc4MDU3NTA1MywiZXhwIjoxNzg4MzUxMDUzLCJhY2NvdW50RXhwaXJlZCI6ZmFsc2UsImFjY291bnRFeHBpcmVzQXQiOjE3ODgzNTEwNTMsImFsbG93ZWRPcmlnaW5zIjpbImh0dHBzOi8vcHN5Y2hlZWVmbGl4dXV1Lm5ldGxpZnkuYXBwIiwiaHR0cHM6Ly9wc3ljaGVlZWZsaXh1dXUudmVyY2VsLmFwcCIsImh0dHBzOi8vcHN5Y2hlZmxpeC5wYWdlcy5kZXYiXX0.ngv2-3w4tcSIZR4J72tEsTZGQYt4iWRdVfFo3sQZa84',
+      url: 'https://psycheflixcignal.csx067.workers.dev/a2z/manifest.mpd',
       type: 'dash',
       drm: {
         scheme: 'clearkey',
